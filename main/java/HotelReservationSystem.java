b4bab71e3292a6d94b56208eaa77fcb8ac337d86
amruta.vatare <vatareamruta615@gmail.com>
2022-09-15 22:07:44 +0530

    UC2 is implemented to find cheap hotel

--- main/java/HotelReservationSystem.java
+++ main/java/HotelReservationSystem.java
@@ -1,4 +1,10 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class HotelReservationSystem {
     static Hotel hotel;
@@ -10,8 +16,60 @@ public class HotelReservationSystem {
     }*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        addDetailsOfHotel();
+        System.out.println("How many hotels you want to add");
+        int ch = sc.nextInt();
+        for(int i = 0 ;i<ch;i++){
+            addDetailsOfHotel();
+        }
         displayHotels();
+        //HotelBooking();
+        getCheapHotel();
+    }
+
+    private static void getCheapHotel() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter date from in format yyyy-MM-dd");
+        String dateFrom = sc.next();
+        System.out.println("Enter date to in format yyyy-MM-dd");
+        String dateTo = sc.next();
+        LocalDate dateF = LocalDate.parse(dateFrom);
+        LocalDate dateT = LocalDate.parse(dateTo);
+        if(isWeekend(dateF) && isWeekend(dateT)){
+            List<Hotel> cheapWeekendHotelLList = HotelMenu.hotels.stream().filter((hotel)->hotel.regularCustomerWeekendRate < 100).toList();
+            System.out.println(cheapWeekendHotelLList);
+        }else{
+            List<Hotel> cheapWeekdayHotelLList = HotelMenu.hotels.stream().filter((hotel)->hotel.regularCustomerWeekendRate < 100).toList();
+            System.out.println(cheapWeekdayHotelLList);
+        }
+
+    }
+
+    private static void HotelBooking() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose hotels");
+        for (int i =0; i<HotelMenu.hotels.size();i++) {
+            System.out.println(i+" :"+HotelMenu.hotels.get(i).name);
+        }
+        int index = sc.nextInt();
+        System.out.println("Enter date from in format yyyy-MM-dd");
+        String dateFrom = sc.next();
+        System.out.println("Enter date to in format yyyy-MM-dd");
+        String dateTo = sc.next();
+        LocalDate dateF = LocalDate.parse(dateFrom);
+        LocalDate dateT = LocalDate.parse(dateTo);
+        if(isWeekend(dateF) && isWeekend(dateT)){
+            int weedEndRate = HotelMenu.hotels.get(index).getRewardCustomerWeekendRate();
+            System.out.println(weedEndRate);
+        }else{
+            int weedDayRate =HotelMenu.hotels.get(index).getRewardCustomerWeekendRate();
+            System.out.println(weedDayRate);
+        }
+
+    }
+    public static boolean isWeekend(final LocalDate ld)
+    {
+        DayOfWeek day = DayOfWeek.of(ld.get(ChronoField.DAY_OF_WEEK));
+        return day == DayOfWeek.SUNDAY || day == DayOfWeek.SATURDAY;
     }
 
     private static void displayHotels() {
